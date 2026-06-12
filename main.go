0dafdeed98f55142caf217b84b480d54d0e8c081
azizu06 <ab725492@ucf.edu>
2026-06-12 18:29:52 -0400

    Add static app and admin metrics

--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+	fileServer := http.FileServer(http.Dir("."))
+	handler := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	log.Fatal(server.ListenAndServe())
 }
